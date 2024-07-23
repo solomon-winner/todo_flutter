f46662d41e620795bbe39a074806b58ac34ab48e
solomon-winner <solyalew3@gmail.com>
2024-07-23 20:38:55 +0300

    ..

--- lib/Model/model.dart
+++ lib/Model/model.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/foundation.dart';
 
 class Item {
+  final int id;
+  final String body;
 
+  Item({
+    required this.id,
+    required this.body,
+  });
+
+  Item copyWith({required int id, required String body}) {
+    return Item(
+      id: id ?? this.id,
+      body: body ?? this.body,
+    );
+  }
 }
 
-class AppState {}
+class AppState {
+  final List<Item> items;
+  
+  AppState({
+    required this.items,
+  });
+
+  AppState.initialState() : items = List.unmodifiable(<Item>[]);
+}
