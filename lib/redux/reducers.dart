4707067f329e1080d79f4293050e255f4744e75a
solomon-winner <solyalew3@gmail.com>
2024-07-23 20:46:23 +0300

    ...

--- lib/redux/reducers.dart
+++ lib/redux/reducers.dart
@@ -1,7 +1,14 @@
 import 'package:todo_flutter/Model/model.dart';
 
 class AddItemAction {
-  
+  static int _id = 0;
+  final String item;
+
+  AddItemAction(this.item) {
+    _id++;
+  }
+
+  int get id => _id;
 }
 
 class RemoveItemAction {
